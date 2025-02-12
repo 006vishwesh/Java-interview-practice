153478a40354a97eab2f95110e33d7fbb6ffb14a
006vishwesh <90088168+006vishwesh@users.noreply.github.com>
2025-02-12 10:55:52 +0530

    Add files via upload

---
+++ PracticeModel/src/test/java/com/model1/PracticeModel/PracticeModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.model1.PracticeModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
